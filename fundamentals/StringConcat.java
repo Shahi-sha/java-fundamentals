aaeac03acbe58b22b2a4d6ce3601c31c2bbfafe0
Shahi-sha <shahiraalthaf7@gmail.com>
2025-07-08 22:40:18 +0530

    Add files via upload

---
+++ fundamentals/StringConcat.java
@@ -0,0 +1,14 @@
+package task;
+import java.util.*;
+public class StringConcat {
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String a=scan.nextLine();
+		String c=scan.nextLine();
+		String b="Technology";
+		System.out.print(a + " " + b + " " + c);
+		
+
+	}
+
+}
---
+++ fundamentals/Welcome.java
@@ -0,0 +1,10 @@
+package task;
+import java.util.*;
+public class Welcome {
+		public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		String a="Welcome";
+		System.out.println(a + " " + s);
+		}
+}
